0b307428d2e37c05d631ec889bfbf824a90842aa
byteframe <byteframe@gmail.com>
2022-01-13 15:08:44 -0500

    git-ignore tar files, and fiddle with vcxsrv bat

--- misc/startxfce4.bat
+++ misc/startxfce4.bat
@@ -1,3 +1,3 @@
-start /B "C:\Program Files\VcXsrv\vcxsrv.exe" "D:\Work\pdl-gnu\misc\euclid.xlaunch"
+start /B "C:\Program Files\VcXsrv\vcxsrv.exe" "C:\Program Files\VcXsrv\euclid.xlaunch"
 start "" /B "C:\Program Files (x86)\pulseaudio-1.1\bin\pulseaudio.exe"
 Arch.exe run "if [ -z \"$(pidof xfce4-session)\" ]; then export DISPLAY=:0.0; export PULSE_SERVER=tcp:127.0.0.1; startxfce4; pkill '(gpg|ssh)-agent'; taskkill.exe /IM vcxsrv.exe; taskkill.exe /IM pulseaudio.exe /F; fi;"
